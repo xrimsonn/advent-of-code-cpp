0b03c90617444a248c368976d37d78259293a157
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-12-02 23:31:50 -0600

    Second problem

--- 2nd/main.cpp
+++ 2nd/main.cpp
@@ -5,16 +5,13 @@
 using namespace std;
 
 int partOne(char const *argv[]) {
-  int maxRed = 12;
-  int maxGreen = 13;
-  int maxBlue = 14;
   int gameIndex;
   int red;
   int green;
   int blue;
   int indexSum = 0;
 
-  string line, l;
+  string line;
   ifstream file;
 
   file.open(argv[1], ios::in);
@@ -31,34 +28,79 @@ int partOne(char const *argv[]) {
         while (getline(gameStream, dataSemicolon, ';')) {
           stringstream bagStream(dataSemicolon);
           string dataComa;
-          red = 0;
-          green = 0;
-          blue = 0;
           while (getline(bagStream, dataComa, ',')) {
-            if (dataComa.find("red") != string::npos)
-              red = stoi(dataComa.substr(0, dataComa.find("red")));
-            if (dataComa.find("green") != string::npos)
-              green = stoi(dataComa.substr(0, dataComa.find("green")));
-            if (dataComa.find("blue") != string::npos)
-              blue = stoi(dataComa.substr(0, dataComa.find("blue")));
+            red = (dataComa.find("red") != string::npos)
+                      ? stoi(dataComa.substr(0, dataComa.find("red")))
+                      : 0;
+            green = (dataComa.find("green") != string::npos)
+                        ? stoi(dataComa.substr(0, dataComa.find("green")))
+                        : 0;
+            blue = (dataComa.find("blue") != string::npos)
+                       ? stoi(dataComa.substr(0, dataComa.find("blue")))
+                       : 0;
             if (red > 12 || green > 13 || blue > 14)
               invalidGameCounter++;
           }
         }
         if (!invalidGameCounter) {
-          // cout << gameIndex << ": Juego valido." <<  '\n';
           indexSum += gameIndex;
         }
       }
     }
   }
 
-  cout << endl;
   file.close();
   return indexSum;
 }
 
+int partTwo(char const *argv[]) {
+  int red, green, blue;
+  int maxRed, maxGreen, maxBlue;
+  int biggestSum = 0;
+
+  string line;
+  ifstream file;
+
+  file.open(argv[1], ios::in);
+  while (getline(file, line)) {
+    stringstream lineStream(line);
+    string dataColon;
+    while (getline(lineStream, dataColon, ':')) {
+      maxRed = 0;
+      maxGreen = 0;
+      maxBlue = 0;
+      if (dataColon.find("Game") == string::npos) {
+        stringstream gameStream(dataColon);
+        string dataSemicolon;
+        while (getline(gameStream, dataSemicolon, ';')) {
+          stringstream bagStream(dataSemicolon);
+          string dataComa;
+          while (getline(bagStream, dataComa, ',')) {
+            if (dataComa.find("red") != string::npos) {
+              red = stoi(dataComa.substr(0, dataComa.find("red")));
+              maxRed = (red > maxRed) ? red : maxRed;
+            }
+            if (dataComa.find("green") != string::npos) {
+              green = stoi(dataComa.substr(0, dataComa.find("green")));
+              maxGreen = (green > maxGreen) ? green : maxGreen;
+            }
+            if (dataComa.find("blue") != string::npos) {
+              blue = stoi(dataComa.substr(0, dataComa.find("blue")));
+              maxBlue = (blue > maxBlue) ? blue : maxBlue;
+            }
+          }
+        }
+      }
+      biggestSum += (maxRed * maxGreen * maxBlue);
+    }
+  }
+
+  file.close();
+  return biggestSum;
+}
+
 int main(int argc, char const *argv[]) {
   printf("Part one: %d\n", partOne(argv));
+  printf("Part two: %d\n", partTwo(argv));
   return 0;
 }
